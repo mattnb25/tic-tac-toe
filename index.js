02bb209d933c96f9d40c6d4c6fdfa12aefcfd213
Matthew <129472095+MNB-87@users.noreply.github.com>
2023-08-15 10:34:06 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,96 @@
+const boardModule = (() => {
+  const button = document.querySelector("#button");
+  const displayBoard = document.querySelector("#board");
+
+  function setupBoard(board, player) {
+    board.forEach((row, rowIndex) => {
+      row.forEach((cell, colIndex) => {
+        const span = document.createElement("span");
+        displayBoard.appendChild(span);
+        span.addEventListener("click", () => {
+          if (board[rowIndex][colIndex] !== "") {
+            return;
+          }
+
+          board[rowIndex][colIndex] = player;
+          span.textContent = board[rowIndex][colIndex];
+
+          if (gameModule.checkWin(player)) {
+            resetRound(board, player);
+            button.textContent = player + " wins! Continue?";
+          } else if (gameModule.checkDraw()) {
+            resetRound(board, player);
+            button.textContent = "It's a draw. Continue?";
+          } else {
+            span.style.pointerEvents = "none";
+            player = player === "X" ? "O" : "X";
+            button.textContent = player + "'s turn";
+          }
+        });
+      });
+    });
+  }
+
+  function resetRound(board, player) {
+    button.addEventListener("click", resetBoard.bind(null, board, player));
+    button.style.pointerEvents = "auto";
+    player = player === "X" ? "O" : "X";
+    displayBoard.style.pointerEvents = "none";
+  }
+
+  function resetBoard(board, player) {
+    button.removeEventListener("click", resetBoard.bind(null, board, player));
+    button.style.pointerEvents = "none";
+    button.textContent = player + "'s turn";
+
+    board.forEach((row) => row.fill(""));
+    displayBoard.innerHTML = "";
+    displayBoard.style.pointerEvents = "auto";
+    setupBoard(board, player);
+  }
+
+  return { resetBoard };
+})();
+
+const gameModule = (() => {
+  let player = "X";
+  const board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+
+  function checkWin(player) {
+    // check rows
+    for (let row of board) {
+      if (row.every((cell) => cell === player)) {
+        return true;
+      }
+    }
+
+    // check columns
+    for (let i = 0; i < board.length; i++) {
+      if (board.every((row) => row[i] === player)) {
+        return true;
+      }
+    }
+
+    // Check diagonals for a win
+    if (
+      board.every((row, index) => row[index] === player) ||
+      board.every((row, index) => row[board.length - 1 - index] === player)
+    ) {
+      return true;
+    }
+
+    return false;
+  }
+
+  function checkDraw() {
+    return board.every((row) => row.every((cell) => cell !== ""));
+  }
+
+  boardModule.resetBoard(board, player); // initialize the game
+
+  return { checkWin, checkDraw };
+})();
